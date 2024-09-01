8e8c4cb86b88eb2ed428bc50fd1838db55432a54
PDempsey03 <dempsey.202@osu.edu>
2024-09-01 12:23:13 -0400

    Added basic perlin noise
    the terrain is now given a 2D array of floats that give the terrain changing height using Perlin noise

--- unity/easyWorld/Assets/Scenes/TerrainGenerator.cs
+++ unity/easyWorld/Assets/Scenes/TerrainGenerator.cs
@@ -25,6 +25,35 @@ public class TerrainGenerator : MonoBehaviour
     {
         terrainData.heightmapResolution = width + 1;
         terrainData.size = new Vector3(width, height, length);
+        terrainData.SetHeights(0, 0, GenerateHeightMap());
+    }
+
+    float[,] GenerateHeightMap()
+    {
+        int heightMapWidth = width + 1;
+        int heightMapLength = length + 1;
+
+        float[,] heightMap = new float[heightMapWidth, heightMapLength];
+
+        for (int x = 0; x < heightMapWidth; x++)
+        {
+            for (int z = 0; z < heightMapLength; z++)
+            {
+                heightMap[x, z] = CalculatePositionHeight(x, z);
+            }
+        }
+
+        return heightMap;
+    }
+
+    float CalculatePositionHeight(int x, int z)
+    {
+        // scale positions [0,1]
+        float xPos = (float)x / width;
+        float zPos = (float)z / length;
+
+        float perlinNoiseHeight = Mathf.PerlinNoise(xPos, zPos);
+        return Mathf.Clamp(perlinNoiseHeight, 0, 1);
     }
 
     void Update()
