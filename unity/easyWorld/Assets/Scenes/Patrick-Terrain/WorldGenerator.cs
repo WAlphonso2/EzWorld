d81d20adf32a9367bdc52d6a3520b3604a14bc21
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-09-04 16:12:43 -0400

    Added Object Generation Outline
    There is now the WorldGenerator class which stores all of the generator scripts. These generator scripts could be for terrain, clouds, weather effects, or even time of day.

---
+++ unity/easyWorld/Assets/Scenes/Patrick-Terrain/WorldGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scenes.Patrick_Terrain
+{
+    public class WorldGenerator : MonoBehaviour
+    {
+        public List<Generator> generators;
+
+        public void GenerateNewWorld(WorldInfo worldInfo)
+        {
+            Debug.Log(worldInfo);
+
+            // tell all generators to clear up anything from previous world
+            generators.ForEach(g => g.Clear());
+
+            // tell all generators to generate their worlds which will run in parallel
+            foreach (Generator g in generators)
+            {
+                StartCoroutine(g.Generate(worldInfo));
+            }
+        }
+    }
+}
