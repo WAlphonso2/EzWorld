58af21e0466d4301f99a118adf413b88d190c2af
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-09-24 14:40:56 -0400

    added basic lighting for day night cycle
    the ai now sets the time of day which rotates the sun in the sky

--- unity/easyWorld/Assets/Scripts/Generators/DayNightGenerator.cs
+++ unity/easyWorld/Assets/Scripts/Generators/DayNightGenerator.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 public class DayNightGenerator : Generator
 {
     public Light sun;
+
+    [Range(0, 24)]
+    public float timeOfDay = 12;
+
     public override IEnumerator Generate(WorldInfo worldInfo)
     {
+        LoadSettings(worldInfo.dayNightGeneratorData);
 
+        SetTimeOfDay(timeOfDay);
 
         yield return null;
     }
 
     public override void Clear()
     {
+        SetTimeOfDay(12); // reset to noon
+    }
 
+    private void SetTimeOfDay(float timeOfDay)
+    {
+        sun.transform.rotation = Quaternion.Euler(Mathf.Lerp(-90, 270, timeOfDay / 24f), 0, 0);
+    }
+
+    private void OnValidate()
+    {
+        SetTimeOfDay(timeOfDay);
+    }
+
+    private void LoadSettings(DayNightGeneratorData data)
+    {
+        timeOfDay = data.timeOfDay;
     }
 }
