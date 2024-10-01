fa7be5bb2de55d59c2415317ea80243c690bb597
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-10-01 17:09:11 -0400

    Added different generator sections
    there are now terrain independent and terrain dependent generator lists. If your generator depends on the terrain and terrain indices, then it should go into the terrain generator.

--- unity/easyWorld/Assets/Scripts/WorldGenerator.cs
+++ unity/easyWorld/Assets/Scripts/WorldGenerator.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    public List<Generator> generators;
-    public CharacterSelectionManager characterSelectionManager;  
+    public List<Generator> terrainDependentGenerators;
+    public List<Generator> terrainIndependentGenerators;
+    public CharacterSelectionManager characterSelectionManager;
 
     public IEnumerator ClearCurrentWorld()
     {
         Debug.Log("Clearing current world");
-        generators.ForEach(g => g.Clear());
+        terrainDependentGenerators.ForEach(g => g?.Clear());
+        terrainIndependentGenerators.ForEach(g => g?.Clear());
         yield return null;
     }
 
     public void GenerateNewWorld(WorldInfo worldInfo)
     {
         // Generate the world and show character selection after terrain generation is complete
-        StartCoroutine(GenerateWorldAndShowCharacterSelection(worldInfo));
+        StartCoroutine(GenerateTerrainDependentWorld(worldInfo));
+        StartCoroutine(GenerateTerrainIndependentWorld(worldInfo));
     }
 
-    private IEnumerator GenerateWorldAndShowCharacterSelection(WorldInfo worldInfo)
+    private IEnumerator GenerateTerrainDependentWorld(WorldInfo worldInfo)
     {
         for (int i = 0; i < worldInfo.terrainsData.Count; i++)
         {
-            foreach (Generator g in generators)
+            foreach (Generator g in terrainDependentGenerators)
             {
                 yield return StartCoroutine(g.Generate(worldInfo, i));
             }
         }
 
+        // show character after the terrain has been finished
+        ShowCharacter(worldInfo);
+    }
+
+    private IEnumerator GenerateTerrainIndependentWorld(WorldInfo worldInfo)
+    {
+        terrainIndependentGenerators.ForEach(g => StartCoroutine(g.Generate(worldInfo)));
+        yield return null;
+    }
+
+    private void ShowCharacter(WorldInfo worldInfo)
+    {
         Debug.Log("Terrain generation complete. Showing character selection UI.");
 
         // Pass the worldInfo to the CharacterSelectionManager
