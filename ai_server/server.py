b760d1e189b104a783312999e5eecadb80e4a477
Mati1514 <matiman1415@gmail.com>
2024-10-21 21:00:49 -0400

    server

--- ai_server/server.py
+++ ai_server/server.py
@@ -175,6 +175,35 @@ def parse_description():
     - waterLevel represents the level of water height for lakes or oceans, should be between 50 and 0.
     - river width range x and y, x should be between 100 and 100, y should be between 100 and 1000 
     - randomize and autoUpdate should be true or false.
+    
+    CityGenerator: if the user wantd city always generate building
+    - If the user mentions generating a city, include the following parameters in the output:
+    - citySize: Choose from [Small, Medium, Large, Very Large]
+    - withSatelliteCity: Boolean, whether the city should have a satellite connected by a highway
+    - borderFlat: Boolean, whether the city should have flat surroundings(Mostly False)
+    - withDowntownArea: Boolean, whether the city should have a downtown metropolitan area
+    - downtownSize: Float 0-200, represents the size of the downtown area
+    - addTrafficSystem: Boolean, whether a traffic system should be added(Mostly True)
+    - trafficHand: Choose between [RightHand, LeftHand]
+    
+    ObjectData:
+    Multiplle copies of objects are allowed to be generated at a time.
+    - name: The name of the object. Must be in {object_set}
+    - x: The x position of the center of the object, 0<x<1024
+    - y: The y position of the center of the object, 0<y<1024
+    - Rx: The rotation of the object around the x axis, 0<Rx<360
+    - Ry: The rotation of the object around the y axis, 0<Ry<360
+    - Rz: The rotation of the object around the z axis, 0<Rz<360
+    - scale: The scale of the model size, as a multiple of the model, 0 < scale < 4, typically 1
+
+    AtmosphereGenerator:
+    - timeOfDay is a floating point value representing the time of day. Its value should be between 0 and 24 inclusive with 0 and 24 representing 12:00am, 12 representing 12:00pm and so on. 
+    - sunSize is a floating point value representing the size of the sun. Its value should range from 0 to 1 inclusive and the standard sun size is .05.
+    - skyTint is a color defined by RGB values each ranging from 0 to 1. Default sky color should be r=.5, g=.5, b=.5
+    - atmosphericThickness is a float ranging from 0-5 inclusive. The standard value is 1.
+    - exposure is a float ranging from 0-8 inclusive. The standard value is 1.3 and this controls the overall light intensity coming from the sun in the skybox
+    - fogIntensity is a float ranging from 0-.5 inclusive. 0 implies no fog and .5 implies extremely foggy. Unless specified, 0 fog intensity should be the default.
+    - fogColor is a color defined by RGB values each ranging from 0 to 1. Default fog color should be r=.5, g=.5, b=.5
 
     Make sure you return the result in JSON format like this:   
     {{
@@ -242,10 +271,49 @@ def parse_description():
                 }}
             }},
             ...
+        ],
+        "atmosphereGeneratorData": {{
+            "timeOfDay": float,
+            "sunSize": float,
+            "skyTint":{{
+                "r": float,
+                "g": float,
+                "b": float
+            }}
+            "atmosphericThickness": float,
+            "exposure": float,
+            "fogIntensity": float,
+            "fogColor": {{
+                "r": float,
+                "g": float,
+                "b": float
+            }}
+        }},
+        "objectList": [
+        {{
+            "name": string,
+            "x": float,
+            "y": float,
+            "Rx": float,
+            "Ry": float,
+            "Rz": float,
+            "scale": float
+        }},
+        ...
         ]
+        "cityData": {{
+            "citySize": string,
+            "withSatelliteCity": boolean,
+            "borderFlat": boolean,
+            "withDowntownArea": boolean,
+            "downtownSize": float,
+            "addTrafficSystem": boolean,
+            "trafficHand": string
+        }},
+
     }}
 
-
+    The structure should be well-formed, and all boolean values should be correctly set.
     Use the following description to generate appropriate values:
     "{description}"
     """
